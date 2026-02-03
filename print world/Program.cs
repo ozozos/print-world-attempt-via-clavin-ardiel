b1064d84264c8443ee6af1b26f61fda47f9cbc3e
362567661 <362567661@2260I-L29-011>
2026-02-03 13:04:37 -0500

    Add project files.

---
+++ print world/Program.cs
@@ -0,0 +1,20 @@
+﻿//This is a comment made from calvin ardiel :)
+//This Comment is under another Comment :))))
+Console.WriteLine("HELO im calvin");
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("         ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("press Enter to continue");
+Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("      CCCCCCCC              A   ");
+Console.WriteLine("     C                     A A  ");
+Console.WriteLine("    C                     A   A ");
+Console.WriteLine("   C                     A     A    ");
+Console.WriteLine("   C                    A       A   " );
+Console.WriteLine("   C                   A         A   ");
+Console.WriteLine("   C                  AAAAAAAAAAAAA   ");
+Console.WriteLine("    C                A             A ");
+Console.WriteLine("     C              A               A  ");
+Console.WriteLine("      CCCCCCCC     A                 A  ");
+Console.Beep();
